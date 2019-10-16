0f172e5acd751b97be2ee3a11261dbbae815aef3
Dimitry Krychylskyy <dimitry.krychylskyy@cgi.com>
2019-10-16 12:51:32 +0200

    INIT Commit

---
+++ src/main/java/org/dk/tuto/maven/App.java
@@ -0,0 +1,13 @@
+package org.dk.tuto.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
